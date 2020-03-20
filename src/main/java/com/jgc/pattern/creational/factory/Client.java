effd440a85862e4c3d58655f856cb8dd34e58267
Jorge García Castaño <jorgegarcia@stratio.com>
2020-03-20 01:21:00 +0100

    refactor

---
+++ src/main/java/com/jgc/pattern/creational/factory/Client.java
@@ -0,0 +1,19 @@
+package com.jgc.pattern.creational.factory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Collection<F16> myAirForce = new ArrayList<>();
+        F16 f16A = new F16A();
+        F16 f16B = new F16B();
+        myAirForce.add(f16A);
+        myAirForce.add(f16B);
+
+        for (F16 f16 : myAirForce) {
+            f16.fly();
+        }
+    }
+}
